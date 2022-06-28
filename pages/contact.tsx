2833e7a6cddfac43a81633e800ed299725ec729a
Lionel Ritchie <lionelrtchieee@gmail.com>
2022-06-28 14:00:50 +0700

    feat: style contact page to be responsive

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -12,19 +12,24 @@ const ContactPage: NextPage = () => {
       </Navbar>
 
       <Layout>
-        <section className='mb-12'>
+        <section className='mb-12 max-w-4xl mx-auto'>
           <h2 className='font-medium text-3xl md:text-4xl mt-20 mb-10'>Call or visit us today.</h2>
         </section>
 
-        <section className='mb-12 font-light max-w-4xl mx-auto leading-7'>
-          <p>
-            Jl. RS Mata Aini No.5, RW.5, unit 1502, Kuningan, Karet, Kecamatan Setiabudi, Kota
-            Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12920
-          </p>
-          <p>+62 857-1797-5770</p>
-          <p className='mt-6'>Monday – Friday: 8AM – 5PM</p>
-          <p>Saturday: 9AM – 5PM</p>
-          <p>Sunday: Closed</p>
+        <section className='mb-12 max-w-4xl mx-auto flex flex-col md:flex-row'>
+          <div className='md:w-1/2 bg-gray-200 rounded h-72'></div>
+          <div className='md:w-1/2 flex items-center justify-center md:px-12 mt-16 md:mt-0'>
+            <div className='font-light leading-7'>
+              <p>
+                Jl. RS Mata Aini No.5, RW.5, unit 1502, Kuningan, Karet, Kecamatan Setiabudi, Kota
+                Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12920
+              </p>
+              <p>+62 857-1797-5770</p>
+              <p className='mt-6'>Monday – Friday: 8AM – 5PM</p>
+              <p>Saturday: 9AM – 5PM</p>
+              <p>Sunday: Closed</p>
+            </div>
+          </div>
         </section>
 
         <section className='mb-24 font-light max-w-4xl mx-auto leading-7'>
